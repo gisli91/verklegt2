eb4fa9312c725d6e42a6192c34c701e343f8ad71
Saevarl <saevarl3@gmail.com>
2022-05-03 21:27:33 +0000

    added nav, static configs and dummy data to template

--- item/views.py
+++ item/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-
+items = [
+    {"name": "Köttur til sölu", "price": 1000, "description": "svartur og grimmur"},
+    {"name": "Antík vatnabátur", "price": 1200000, "description": "Flýtur"}
+]
 # Create your views here.
 def index(request):
-    return render(request, "item/index.html")
+    return render(request, "item/index.html", context={"items": items})
