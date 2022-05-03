6d3f196edc24111b681d7b0ada3a308f67ce342b
Saevarl <saevarl3@gmail.com>
2022-05-03 21:51:46 +0000

    added simple css, and started working on item models

--- item/models.py
+++ item/models.py
@@ -4,3 +4,13 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    highest_bid = models.FloatField()
+    # seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ItemImage(models.Model):
+    image = models.CharField(max_length=9999)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+
