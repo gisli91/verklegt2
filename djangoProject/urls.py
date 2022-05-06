bf735d605054f8705998c3218488685f040ca97d
Saevarl <saevarl3@gmail.com>
2022-05-06 18:57:03 +0000

    added a signal so that a profile is created for user that registers

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +26,8 @@ urlpatterns = [
     path("user/", include("user.urls")),
     path("", views.frontpage, name="frontpage")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
