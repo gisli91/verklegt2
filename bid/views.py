2c5fd2772da82be91ae7bc059ab98c707f1f3b6e
Saevarl <saevarl3@gmail.com>
2022-05-09 12:55:30 +0000

    bid validation

--- bid/views.py
+++ bid/views.py
@@ -10,25 +10,30 @@ from bid.forms.bid_forms import BidForm
 
 def make_bid(request, id):
     item = get_object_or_404(Item, pk=id)
+    item.highest_bid
     if request.method == "POST":
-        data = request.POST.copy()
-        data['item'] = item
-        form = BidForm(data)
+        form = BidForm(request.POST)
         if form.is_valid():
             bid = form.save(commit=False)
-            bid.bidder = request.user
-            bid.item_id = item.id
-            bid.save()
-            messages.success(request,
-                             f"Your bid of ${bid.bid_amount} for Item: {item.name}, has been successfully placed!")
-            item.highest_bid = bid.bid_amount
-            item.save()
-            return redirect("item-index")
-    else:
-        bid = BidForm()
+            if bid.bid_amount >= item.highest_bid * 1.05:
+                bid.bidder = request.user
+                bid.item_id = item.id
+                bid.save()
+                messages.success(request,
+                                 f"Your bid of ${bid.bid_amount} for Item: {item.name}, has been successfully placed!")
+                item.highest_bid = bid.bid_amount
+                item.save()
+                return redirect("item-index")
+            else:
+                messages.error(request,f"Minimum bid amount is {item.highest_bid * 1.05}")
+                return redirect("make_bid", id=id)
+
+
+    bid = BidForm()
 
     context = {
         "form": bid,
         "item": item,
     }
     return render(request, "bid/make_bid.html", context)
+
