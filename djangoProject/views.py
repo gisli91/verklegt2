53cb61f75b9801fde1a0d78116610d110c67aea3
Saevarl <saevarl3@gmail.com>
2022-05-12 22:42:35 +0000

    latest items READY

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 
 
 def frontpage(request):
-    newest_items = Item.objects.all().order_by("date_posted")
+    latest_items = [{
+        "id": x.id,
+        "name": x.name,
+        "seller": x.seller.username,
+        "highest_bid": x.highest_bid,
+        "category": x.category,
+        "item_image_url": x.item_image.url
+    } for x in Item.objects.all().order_by("-date_posted")]
     return render(request, "frontpage.html",{
-        "newest_items": newest_items
+        "newest_items": latest_items[:3]
     })
