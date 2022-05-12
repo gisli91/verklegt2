f345687c8f1c437d6c5e12a630955686334ff6b5
Saevarl <saevarl3@gmail.com>
2022-05-12 15:22:28 +0000

    bio is now text area

--- user/forms/user_forms.py
+++ user/forms/user_forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import Textarea
+
 from user.models import Profile
 
 class UserSignupForm(UserCreationForm):
@@ -20,4 +22,6 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["bio", "profile_image"]
-
+        widgets = {
+            "bio": Textarea(attrs={'rows': 12})
+        }
