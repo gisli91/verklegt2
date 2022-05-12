17b2c6a51a1afc23436efd8f6329986d86e93251
Saevarl <saevarl3@gmail.com>
2022-05-12 19:45:28 +0000

    bla

--- static/js/items.js
+++ static/js/items.js
@@ -6,7 +6,22 @@ $(document).ready(function () {
             url: "/items?search_filter=" + searchText,
             type: "GET",
             success: function(resp) {
-                    newHTML = createMap(resp)
+                let newHTML = resp.data.map(d => {
+                    return `<div class="col-sm-4">
+                                <a href="/items/${ d.id }">
+                                    <div class="card" style="width: 18rem;">
+                                         <img class="card-img-top" src="${ d.image }">
+                                         <div class="card-body">
+                                            <h5 class="card-title">${ d.name }</h5>
+                                            <p class="card-text">${ d.highest_bid }$</p>
+                                            <p class="card-text"><small class="text-muted">${ d.seller }</small></p>
+                                            <p class="card-text"><small class="text-muted">Category: ${ d.category } </small></p>
+                                         </div>
+                                    </div>
+                                </a>
+                            </div>`
+                    });
+
                     $(".items").html(newHTML.join(""))
                     $('#search-box').val("")
                 },
@@ -48,23 +63,5 @@ $(document).ready(function () {
 
         });
 
-    function createMap(resp) {
-        let newHTML = resp.data.map(d => {
-            return `<div class="col-sm-4">
-                       <a href="/items/${ d.id }">
-                       <div class="card" style="width: 18rem;">
-                           <img class="card-img-top" src="${ d.image }">
-                                <div class="card-body">
-                                    <h5 class="card-title">${ d.name }</h5>
-                                    <p class="card-text">${ d.highest_bid }$</p>
-                                    <p class="card-text"><small class="text-muted">${ d.seller }</small></p>
-                                    <p class="card-text"><small class="text-muted">Category: ${ d.category } </small></p>
-                                </div>
-                       </div>
-                       </a>
-                  </div>`
-        });
-
-}
 });
 
